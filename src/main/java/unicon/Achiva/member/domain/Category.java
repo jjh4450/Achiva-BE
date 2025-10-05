ce892a42997364681951328c646ab6d3a215329a
Jeje <jjh4450jjh4450@gmail.com>
2025-10-06 02:29:15 +0900

    feat: enum에 대해서 안전한 변환 추가

--- src/main/java/unicon/Achiva/member/domain/Category.java
+++ src/main/java/unicon/Achiva/member/domain/Category.java
@@ -1,5 +1,7 @@
 package unicon.Achiva.member.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Getter;
 
 import java.util.Arrays;
@@ -22,14 +24,20 @@ public enum Category {
         this.description = description;
     }
 
+    @JsonCreator
     public static Category fromDisplayName(String name) {
         return Arrays.stream(values())
-                .filter(c -> c.description.equals(name))
+                .filter(c -> c.description.equals(name) || c.name().equalsIgnoreCase(name))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("잘못된 카테고리: " + name));
     }
 
     public static String getDisplayName(Category category) {
         return category.getDescription();
+    }
+
+    @JsonValue
+    public String toValue() {
+        return this.description;
     }
 }
